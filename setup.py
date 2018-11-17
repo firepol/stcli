773c68855488e0625413e7f6df812636b6665615
firepol <1702718+firepol@users.noreply.github.com>
2018-11-17 12:03:56 +0100

    fix packaging and .gitignore

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='stcli',
-      version='0.14',
-      description='stcli - a repl command line crypto wallet for stellar that is simple and all in one file',
-      url='http://github.com/antb123/stcli',
+      version='0.15',
+      description='firepol fork of stcli - a repl command line crypto wallet for stellar that is '
+                  'simple and all in one file - original repo: http://github.com/antb123/stcli',
+      url='http://github.com/firepol/stcli',
       author='antb123 on stellar-public',
       license='MIT',
       packages=['stcli'],
---
+++ stcli/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'stcli'
+__author__ = 'firepol'
+__repo__ = 'https://github.com/firepol/stcli'
+__license__ = 'MIT'
+
+from .stcli import *
